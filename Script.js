8e0e85e5ceb4c4f49c56764049e559e675f9a7bf
Hemanth Gamasu <HemanthGamasu999@gmail.com>
2025-03-02 01:29:18 +0530

    Push 2

--- Script.js
+++ Script.js
@@ -1,21 +1,53 @@
-// Set the date we're counting down to
 var countDownDate = new Date("March 20, 2025 10:00:00").getTime();
+var timerElement = document.querySelector(".Timer-Style");
+const textElement = document.getElementById("dynamic-text");
+const phrases = ["THRITHI 2K25","Event Starts In"];
+let index = 0;
+let charIndex = 0;
+let isTyping = true;
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
+function updateTimer() {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
   
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
-  document.querySelector(".Timer-Style").innerHTML = days + "d " + hours + "h " 
-  + minutes + "m " + seconds + "s ";
-  
-  if (distance < 0) {
-    clearInterval(x);
-    document.querySelector(".Timer-Style").innerHTML = "EXPIRED";
-  }
-}, 1000);
+    if (distance < 0) {
+        timerElement.innerHTML = "EXPIRED";
+    } else {
+        timerElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    }
+}
+
+// Ensure timer is updated every second
+setInterval(updateTimer, 1000);
+updateTimer();
+
+// Separate text container from timer to prevent movement
+function typeEffect() {
+    if (charIndex < phrases[index].length && isTyping) {
+        textElement.innerHTML = phrases[index].substring(0, charIndex + 1);
+        charIndex++;
+        setTimeout(typeEffect, 100);
+    } else {
+        isTyping = false;
+        setTimeout(eraseEffect, 1500);
+    }
+}
+
+function eraseEffect() {
+    if (charIndex > 0 && !isTyping) {
+        textElement.innerHTML = phrases[index].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(eraseEffect, 50);
+    } else {
+        isTyping = true;
+        index = (index + 1) % phrases.length;
+        setTimeout(typeEffect, 500);
+    }
+}
+
+typeEffect();
